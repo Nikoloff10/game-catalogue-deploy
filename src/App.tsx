e722b6e18051a99af31b98ad520195a80fe0be17
Nikolay Nikolov <nikoloff.ftw@gmail.com>
2025-01-23 01:59:09 +0200

    Added highlight upon genre selection

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
         
         
       >
-        <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+        <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)}/>
       </GridItem>
 
       <GridItem area="main">
