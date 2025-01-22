b7a0dadacd4d73b152ac0403e8950505d5206cb5
Nikolay Nikolov <nikoloff.ftw@gmail.com>
2025-01-22 20:53:57 +0200

    Add loading skeletons to gamecards

--- src/mycomponents/GameCard.tsx
+++ src/mycomponents/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "@/hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "@/services/image-url";
 
 interface Props {
   game: Game;
@@ -9,8 +10,8 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card.Root borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+    <Card.Root borderRadius={10} overflow="hidden" height='340px'>
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
---
+++ src/mycomponents/GameCardSkeleton.tsx
@@ -0,0 +1,18 @@
+import { Card, CardBody, HStack, Skeleton } from "@chakra-ui/react";
+
+const GameCardSkeleton = () => {
+  return (
+    <Card.Root height="340px" borderRadius={10} overflow="hidden">
+      <Skeleton height="200px" />
+      <CardBody gapY={4}>
+        <Skeleton height="20px" width="80%" />
+        <HStack justifyContent="space-between">
+          <Skeleton height="20px" width="30%" />
+          <Skeleton height="20px" width="10%" />
+        </HStack>
+      </CardBody>
+    </Card.Root>
+  );
+};
+
+export default GameCardSkeleton;
